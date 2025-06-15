65389e1f9951c5d480b96eb218aa193d2c307c69
chandrawelim <chandra.welim@tuntun.co.id>
2025-06-15 21:19:31 +0700

    feat: initialize GithubFollowers framework with project and test files
    This commit adds the initial structure for the GithubFollowers framework,
    including the project configuration, workspace settings, and basic header
    and test files. This setup is essential for starting development and
    testing of the framework functionalities.

---
+++ GithubFollowers/GithubFollowers/GithubFollowers.h
@@ -0,0 +1,18 @@
+//
+//  GithubFollowers.h
+//  GithubFollowers
+//
+//  Created by Chandra Welim on 15/06/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubFollowers.
+FOUNDATION_EXPORT double GithubFollowersVersionNumber;
+
+//! Project version string for GithubFollowers.
+FOUNDATION_EXPORT const unsigned char GithubFollowersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubFollowers/PublicHeader.h>
+
+
