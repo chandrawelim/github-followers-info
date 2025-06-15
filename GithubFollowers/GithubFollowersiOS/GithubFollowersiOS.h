1ae874df40f5c4b1efd8bd05d8e4817dcd0c06a5
chandrawelim <chandra.welim@tuntun.co.id>
2025-06-15 22:42:54 +0700

    feat(GithubFollowersiOS): add initial header file for the GithubFollowersiOS framework feat(GithubFollowersiOSTests): add initial test suite for the GithubFollowersiOS framework

---
+++ GithubFollowers/GithubFollowersiOS/GithubFollowersiOS.h
@@ -0,0 +1,18 @@
+//
+//  GithubFollowersiOS.h
+//  GithubFollowersiOS
+//
+//  Created by Chandra Welim on 15/06/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubFollowersiOS.
+FOUNDATION_EXPORT double GithubFollowersiOSVersionNumber;
+
+//! Project version string for GithubFollowersiOS.
+FOUNDATION_EXPORT const unsigned char GithubFollowersiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubFollowersiOS/PublicHeader.h>
+
+
