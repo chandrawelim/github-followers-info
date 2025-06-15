aca7878aee66d99a0ff0d2d2b9275a9097681d9c
chandrawelim <chandra.welim@tuntun.co.id>
2025-06-15 22:51:15 +0700

    feat(UIView+Ext.swift): add UIView extension file for future UI enhancements and code organization

---
+++ GithubFollowers/GithubFollowersiOS/Views/UIView+Ext.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Ext.swift
+//  GithubFollowersiOS
+//
+//  Created by Chandra Welim on 15/06/25.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+        ])
+    }
+    
+    func addSubviews(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+}
+
